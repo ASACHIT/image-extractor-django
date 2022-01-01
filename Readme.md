115768c10487077c40b27888f7772b18a73dc3a9
Sachit Yadav <73944456+ASACHIT@users.noreply.github.com>
2022-01-01 18:44:46 +0545

    enchanced UI,add download button & some fixes

--- Readme.md
+++ Readme.md
@@ -1,22 +1,19 @@
 
-# Image extractor 
+# Image extractor
 
 ### A simple image extractor webapp in django
 
 ## Installation
+
 **Running Code directly**
+
 ```bash
   git clone https://github.com/ASACHIT/image-extractor-django.git
   cd image-extractor-django
   pip3 install -r requirements.txt
   python3 manager.py runserver
 ```
-    
-## Screenshots
+
+## Screenshot
+
 ![App Screenshot](https://cdn.discordapp.com/attachments/881148108602503218/883959833550802994/unknown.png)
-  
-## Demo
-
-Demo here - https://www.facebook.com/100024256128513/videos/269177531486237/
-
-  
--- img/views.py
+++ img/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import redirect, render
-from django.http import HttpResponse, HttpRequest
-import wget
+from django.http import HttpRequest
+
 
 from .imgextractor import Assets
 
@@ -9,11 +9,10 @@ def index(request: HttpRequest):
 
     if request.method == "POST":
         url = request.POST.get("link")
-        if url.startswith("https"):
+        if url.startswith("https") or url.startswith("http"):
             print(url)
             get_images = Assets(url)
             links_ = get_images.pull_images()
-
             return render(request, "index.html", context={"imageslink": links_})
         else:
             return redirect("homepage")
